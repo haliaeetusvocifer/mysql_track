32f90c3309ae98f420a902eeac8f16baaec92a6d
gitsegun <segun@dealdey.com>
2016-10-17 09:53:40 +0100

    Alter table changes by removing comments and indenting

--- alter_table/alter_table_exercise.sql
+++ alter_table/alter_table_exercise.sql
@@ -6,11 +6,17 @@
 
 		mysql -u root -p
 
-		CREATE DATABASE testone;										[This creates database testone]	
-		USE testone;													[This switch to the database testone]											 											
-		CREATE TABLE testing_table (name VARCHAR(100), contact_name VARCHAR(100), roll_no VARCHAR(10));			[This creates name, contact_name and roll_no qith their datatypes]
+		CREATE DATABASE testone;
 
-		DESCRIBE testing_table;      [This shows me the columns in the table andd their datatype]
+		USE testone;	
+
+		CREATE TABLE testing_table (
+			name VARCHAR(100),
+			contact_name VARCHAR(100),
+			roll_no VARCHAR(10)
+			);			
+
+		      
 
 
 
@@ -18,7 +24,7 @@
 	
 	COMMAND:
 
-		ALTER TABLE testing_table DROP name;        [Delete column name]
+		ALTER TABLE testing_table DROP name;       
 
 
 
@@ -33,7 +39,10 @@
 
 	COMMAND:
 
-		ALTER TABLE testing_table ADD (first_name VARCHAR(100), last_name VARCHAR(60));
+		ALTER TABLE testing_table ADD (
+			first_name VARCHAR(100), 
+			last_name VARCHAR(60)
+			);
 
 
 5. Change the type of roll_no to integer
