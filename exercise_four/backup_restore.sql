254b81cf0941ca62694272a71af31272f408742a
gitsegun <segun@dealdey.com>
2016-10-17 10:38:26 +0100

    commit indetation for backup_restore.sql

--- exercise_four/backup_restore.sql
+++ exercise_four/backup_restore.sql
@@ -1,10 +1,6 @@
- Backup and Restore :Take backup of the db you created for practice/exercise and send it to me. Now create a new. database named "restored" and restore that backup into this DB.
 
+mysqldump -u root -p vtapp > vtapp_backup.sql
 
- 	COMMANDS:
+ 	CREATE DATABASE restored;
 
- 		mysqldump -u root -p vtapp > vtapp_backup.sql
-
- 		CREATE DATABASE restored;
-
- 		mysqldump -u root -p restored < vtapp_backup.sql
+ 	mysqldump -u root -p restored < vtapp_backup.sql
