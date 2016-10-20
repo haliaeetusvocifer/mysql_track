0191d598fba4aaefee7c8676b7cc74c02c2cb469
gitsegun <segun@dealdey.com>
2016-10-20 11:52:27 +0100

    third indentation done

--- exercise_five/mail_campaign.sql
+++ exercise_five/mail_campaign.sql
@@ -1,31 +1,29 @@
 
 
-	CREATE DATABASE mail_campaign;
+  CREATE DATABASE mail_campaign;
 
-	USE mail_campaign;
+  USE mail_campaign;
 
-	CREATE TABLE subscribers (
+  CREATE TABLE subscribers (
   email_address VARCHAR(100) NOT NULL, 
   mobile_number INT UNSIGNED, 
   city VARCHAR(50) NOT NULL
   );
 
 
-	LOAD DATA LOCAL INFILE 'email_subscribers.txt' 
+  LOAD DATA LOCAL INFILE 'email_subscribers.txt' 
   INTO TABLE subscribers FIELDS TERMINATED BY ',';
 
 
-	SELECT * 
+  SELECT * 
   FROM subscribers;
 
 
-
-
 /*
-What all cities did people respond from 
+What all cities did people respond from
 */
 
-	SELECT DISTINCT city 
+  SELECT DISTINCT city 
   FROM subscribers;
 
 
@@ -35,7 +33,7 @@ What all cities did people respond from
 How many people responded from each city
 */
 
-	SELECT city, COUNT(*) 
+  SELECT city, COUNT(*) 
   FROM subscribers 
   GROUP BY city; 
 
@@ -46,7 +44,7 @@ How many people responded from each city
 Which city were the maximum respondents from?
 */
 
-	SELECT city, COUNT(*) AS cityCount 
+  SELECT city, COUNT(*) AS cityCount 
   FROM subscribers 
   GROUP BY city 
   ORDER BY cityCount 
@@ -76,5 +74,3 @@ Which is the most popular email domain among the respondents ?
   GROUP BY substring_index(email_address, '@', -1) 
   ORDER BY MyCount 
   DESC LIMIT 1;
-
-
