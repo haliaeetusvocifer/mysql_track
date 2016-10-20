e0b98d471bd47a58772772c672418c598a676f9b
gitsegun <segun@dealdey.com>
2016-10-20 11:31:00 +0100

    proper indentation according to mysql study guide

--- exercise_five/mail_campaign.sql
+++ exercise_five/mail_campaign.sql
@@ -4,25 +4,29 @@
 
 	USE mail_campaign;
 
-	CREATE TABLE subscribers (email_address VARCHAR(100) NOT NULL, 
-    mobile_number INT UNSIGNED, city VARCHAR(50) NOT NULL
-    );
+	CREATE TABLE subscribers (
+  email_address VARCHAR(100) NOT NULL, 
+  mobile_number INT UNSIGNED, 
+  city VARCHAR(50) NOT NULL
+  );
 
 
 	LOAD DATA LOCAL INFILE 'email_subscribers.txt' 
-    INTO TABLE subscribers FIELDS TERMINATED BY ',';
+  INTO TABLE subscribers FIELDS TERMINATED BY ',';
 
 
-	SELECT * FROM subscribers;
+	SELECT * 
+  FROM subscribers;
 
 
 
 
-  /*
-  What all cities did people respond from
-  */
+/*
+What all cities did people respond from 
+*/
 
-	SELECT DISTINCT city FROM subscribers;
+	SELECT DISTINCT city 
+  FROM subscribers;
 
 
 
@@ -31,7 +35,9 @@
 How many people responded from each city
 */
 
-	SELECT city, COUNT(*) FROM subscribers GROUP BY city; 
+	SELECT city, COUNT(*) 
+  FROM subscribers 
+  GROUP BY city; 
 
 
 
@@ -40,9 +46,11 @@ How many people responded from each city
 Which city were the maximum respondents from?
 */
 
-	SELECT city, COUNT(*) AS cityCount FROM subscribers 
-
-    GROUP BY city ORDER BY cityCount DESC LIMIT 1; 
+	SELECT city, COUNT(*) AS cityCount 
+  FROM subscribers 
+  GROUP BY city 
+  ORDER BY cityCount 
+  DESC LIMIT 1; 
 
 
 
@@ -51,9 +59,9 @@ Which city were the maximum respondents from?
 What all email domains did people respond from ?
 */
 
-SELECT DISTINCT substring_index(email_address, '@', -1)domain 
-
-  FROM mail_campaign.subscribers ORDER BY domain;
+  SELECT DISTINCT substring_index(email_address, '@', -1)domain 
+  FROM mail_campaign.subscribers 
+  ORDER BY domain;
 
 
 
@@ -62,10 +70,11 @@ SELECT DISTINCT substring_index(email_address, '@', -1)domain
 Which is the most popular email domain among the respondents ?
 */
 
-SELECT DISTINCT substring_index(email_address, '@', -1)domain, 
-
-  COUNT(*) AS MyCount FROM mail_campaign.subscribers 
-    GROUP BY substring_index(email_address, '@', -1) 
-    ORDER BY MyCount DESC LIMIT 1;
+  SELECT DISTINCT substring_index(email_address, '@', -1)domain, 
+  COUNT(*) AS MyCount 
+  FROM mail_campaign.subscribers 
+  GROUP BY substring_index(email_address, '@', -1) 
+  ORDER BY MyCount 
+  DESC LIMIT 1;
 
 
