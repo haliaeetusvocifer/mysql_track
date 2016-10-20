486c525201e9309d4a79c074140856a4ec0f4a8f
gitsegun <segun@dealdey.com>
2016-10-20 10:33:05 +0100

    COmmit for putting question number as comments

--- exercise_five/mail_campaign.sql
+++ exercise_five/mail_campaign.sql
@@ -13,18 +13,59 @@
     INTO TABLE subscribers FIELDS TERMINATED BY ',';
 
 
-	SELECT * FROM subscribers;   
+	SELECT * FROM subscribers;
+
+
+
+
+  /*
+  What all cities did people respond from
+  */
 
 	SELECT DISTINCT city FROM subscribers;
 
 
+
+
+/*
+How many people responded from each city
+*/
+
 	SELECT city, COUNT(*) FROM subscribers GROUP BY city; 
 
 
-	SELECT MAX(city) FROM subscribers; 
 
+
+/*
+Which city were the maximum respondents from?
+*/
 
 	SELECT city, COUNT(*) AS cityCount FROM subscribers 
+
     GROUP BY city ORDER BY cityCount DESC LIMIT 1; 
+
+
+
+    
+/*
+What all email domains did people respond from ?
+*/
+
+SELECT DISTINCT substring_index(email_address, '@', -1)domain 
+
+  FROM mail_campaign.subscribers ORDER BY domain;
+
+
+
+
+/*
+Which is the most popular email domain among the respondents ?
+*/
+
+SELECT DISTINCT substring_index(email_address, '@', -1)domain, 
+
+  COUNT(*) AS MyCount FROM mail_campaign.subscribers 
+    GROUP BY substring_index(email_address, '@', -1) 
+    ORDER BY MyCount DESC LIMIT 1;
 
 
